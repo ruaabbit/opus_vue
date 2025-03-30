0a29e3957268cd8b11bd147cda3346f7f9ca25da
Ruabbit <liuzhao6526@gmail.com>
2025-03-30 14:22:38 +0800

    misc: update globe

--- src/views/RealTimeDailyForecastView.vue
+++ src/views/RealTimeDailyForecastView.vue
@@ -7,7 +7,14 @@
           <LoadingAnimation />
         </div>
         <div v-else>
-          <SeaIceGlobe :images="images" />
+          <GlobeChartEchartsGL
+            class="globe-chart"
+            :baseTexture="baseTexture"
+            :auto-rotate="true"
+            :target-coord="[0, 90]"
+            :alpha="60"
+            :distance="200"
+          />
         </div>
       </div>
     </div>
@@ -18,14 +25,24 @@
 import { ref, onMounted } from 'vue'
 import { useRealtimeDayPrediction } from '@/common/api'
 import LoadingAnimation from '../components/LoadingAnimation.vue'
-import SeaIceGlobe from '../components/SeaIceGlobe.vue'
+import GlobeChartEchartsGL from '../components/GlobeChartEchartsGL.vue'
 
 const images = ref([])
 const isLoading = ref(true)
 
+// 设置地球基础纹理，也可以根据返回的数据动态设置
+const baseTexture = ref('')
+
 onMounted(() => {
   useRealtimeDayPrediction().then((data) => {
-    images.value = data
+    images.value = data.images
+
+    // 如果API返回的数据中包含海冰覆盖图像URL，可以设置为地球纹理
+    if (data.images && data.images.length > 0 && data.images[0].path) {
+      // 设置第一个图像为地球纹理，如果需要的话
+      baseTexture.value = 'https://seaice.52lxy.one:20443' + data.images[0].path
+    }
+
     isLoading.value = false
   })
 })
@@ -52,5 +69,10 @@ onMounted(() => {
   justify-content: center;
   width: 100%;
   height: 80vh; /* 调整高度以适应地球 */
+}
+
+.globe-chart {
+  width: 80vw;
+  height: 80vh;
 }
 </style>
